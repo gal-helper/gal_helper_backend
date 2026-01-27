f6c3b96609875f7f12513efb840a4565ee2c964a
aya <1726311062@qq.com>
2026-01-27 22:10:58 +0800

    logging的配置修改，添加创建logs文件夹的代码

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,17 +1,19 @@
 import logging
-from config import config
+import os
+from .config import config
+
 
 def setup_logging():
-    # 屏幕/docker上的日志
+    log_dir = os.path.dirname(config.LOG_FILE_NAME)
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir, exist_ok=True)
+
+    # 打印到控制台和文件里
     stream_handler = logging.StreamHandler()
-    # 文件日志
     file_handler = logging.FileHandler(config.LOG_FILE_NAME)
 
     logging.basicConfig(
         level=config.LOG_LEVEL,
         format=config.LOG_FORMAT,
-        handlers=[
-            stream_handler,
-            file_handler
-        ]
+        handlers=[stream_handler, file_handler],
     )
