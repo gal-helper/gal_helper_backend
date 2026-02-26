99f1c1dfe6ebb9e818c366484c448fa6d29573b5
兰斯的超级兵器 <2416910961@qq.com>
2026-02-26 16:53:17 +0800

    Delete api directory

--- api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import chat, chat_session
-from fastapi import APIRouter
-
-api_router = APIRouter()
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
-api_router.include_router(chat_session.router, prefix="/chat_session", tags=["chat_session"])
