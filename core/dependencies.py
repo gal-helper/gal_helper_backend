844f336e655e11214e48d3ba640249ffda2ad14c
兰斯的超级兵器 <2416910961@qq.com>
2026-02-26 16:53:40 +0800

    Delete core directory

--- core/dependencies.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.db import async_db_manager
-
-async def get_db() -> AsyncSession:
-    """统一数据库会话依赖"""
-    async with async_db_manager.get_async_db() as session:
-        yield session
